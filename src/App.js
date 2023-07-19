616fd02392983431454c2b832528911bdef611eb
Matheus Maia <matheusmaia@ficusconsultoria.com.br>
2023-07-19 16:28:29 -0300

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
+import React from 'react';
+import About from './components/About';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Testimonials from './components/Testimonials';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">        
-        <h1>Curriculo Matheus</h1>
-      </header>
-    </div>
+    <main className='text-gray-400 bg-gray-900 body-font'>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,36 @@
+
+
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "Api for React",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "Java",
+      subtitle: "Java Projects for university",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Scripts",
+      subtitle: "Python scripts",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
